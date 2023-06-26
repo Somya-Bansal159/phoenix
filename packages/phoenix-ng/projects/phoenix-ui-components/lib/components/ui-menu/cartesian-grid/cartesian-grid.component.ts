9beffb80a3231603acaedd63536c218e19cfe171
Somya Bansal <somya.bansal.cse20@itbhu.ac.in>
2023-06-26 14:33:03 +0530

    Update cartesian-grid.component.ts

--- packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/cartesian-grid/cartesian-grid.component.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/cartesian-grid/cartesian-grid.component.ts
@@ -1,74 +1,19 @@
-import { Component, OnDestroy, ViewChild } from '@angular/core';
+import { ViewChild } from '@angular/core';
 import { MatCheckbox, MatCheckboxChange } from '@angular/material/checkbox';
 import { MatMenuTrigger } from '@angular/material/menu';
-import { EventDisplayService } from 'projects/phoenix-ui-components/lib/services/event-display.service';
 import { Subscription } from 'rxjs';
 import { Vector3 } from 'three';
 
-@Component({
-  selector: 'app-cartesian-grid',
-  templateUrl: './cartesian-grid.component.html',
-  styleUrls: ['./cartesian-grid.component.scss'],
-})
-export class CartesianGridComponent implements OnDestroy {
   @ViewChild(MatMenuTrigger) gridOptionsTrigger: MatMenuTrigger;
-  @ViewChild(MatCheckbox) shiftCartesianGridCheckbox: MatCheckbox;
 
-  showCartesianGrid: boolean = false;
-  showXY: boolean = true;
-  showYZ: boolean = true;
-  showZX: boolean = true;
-  xDistance: number = 0;
-  yDistance: number = 0;
-  zDistance: number = 0;
-  sparsity: number = 1;
+  
   cartesianPos = new Vector3(0, 0, 0);
 
   originChangedSub: Subscription = null;
   stopShiftingSub: Subscription = null;
 
-  constructor(private eventDisplay: EventDisplayService) {}
 
-  setCartesianGrid(change: MatCheckboxChange) {
-    this.showCartesianGrid = change.checked;
-    this.callSetShowCartesianGrid();
-  }
-
-  addXYPlanes(zDistance: number) {
-    this.zDistance = zDistance;
-    this.callSetShowCartesianGrid();
-  }
-
-  addYZPlanes(xDistance: number) {
-    this.xDistance = xDistance;
-    this.callSetShowCartesianGrid();
-  }
-
-  addZXPlanes(yDistance: number) {
-    this.yDistance = yDistance;
-    this.callSetShowCartesianGrid();
-  }
-
-  changeSparsity(sparsity: number) {
-    this.sparsity = sparsity;
-    this.callSetShowCartesianGrid();
-  }
-
-  showXYPlanes(change: MatCheckboxChange) {
-    this.showXY = change.checked;
-    this.callSetShowCartesianGrid();
-  }
-
-  showYZPlanes(change: MatCheckboxChange) {
-    this.showYZ = change.checked;
-    this.callSetShowCartesianGrid();
-  }
-
-  showZXPlanes(change: MatCheckboxChange) {
-    this.showZX = change.checked;
-    this.callSetShowCartesianGrid();
-  }
-
+  
   shiftCartesianGridByPointer(change: MatCheckboxChange) {
     this.eventDisplay
       .getUIManager()
@@ -110,23 +55,7 @@ export class CartesianGridComponent implements OnDestroy {
     this.eventDisplay.getUIManager().showLabels(change.checked);
   }
 
-  callSetShowCartesianGrid() {
-    this.eventDisplay
-      .getUIManager()
-      .setShowCartesianGrid(
-        this.showCartesianGrid,
-        this.showXY,
-        this.showYZ,
-        this.showZX,
-        this.xDistance,
-        this.yDistance,
-        this.zDistance,
-        this.sparsity
-      );
-  }
 
-  ngOnDestroy(): void {
+
     if (this.originChangedSub != null) this.originChangedSub.unsubscribe();
     if (this.stopShiftingSub != null) this.stopShiftingSub.unsubscribe();
-  }
-}
