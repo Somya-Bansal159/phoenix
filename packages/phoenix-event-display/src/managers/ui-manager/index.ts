7fd11790a0edad1d885535cca130a0d31b962c70
Somya Bansal <somya.bansal.cse20@itbhu.ac.in>
2023-06-26 14:13:01 +0530

    Update index.ts

--- packages/phoenix-event-display/src/managers/ui-manager/index.ts
+++ packages/phoenix-event-display/src/managers/ui-manager/index.ts
@@ -391,34 +391,6 @@ export class UIManager {
   }
 
   /**
-   * Set whether to show the cartesian or not
-   * @param show If the grid is to be shown or not.
-   */
-  public setShowCartesianGrid(
-    show: boolean,
-    showXY: boolean,
-    showYZ: boolean,
-    showZX: boolean,
-    xDistance: number,
-    yDistance: number,
-    zDistance: number,
-    sparsity: number
-  ) {
-    this.three
-      .getSceneManager()
-      .setCartesianGrid(
-        show,
-        showXY,
-        showYZ,
-        showZX,
-        xDistance,
-        yDistance,
-        zDistance,
-        sparsity
-      );
-  }
-
-  /**
    * Show labels on cartesian grid
    */
   public showLabels(visible: boolean) {
@@ -443,7 +415,7 @@ export class UIManager {
    * Set whether to show the eta/phi or not
    * @param show If the grid is to be shown or not.
    */
-  public setShowEtaPhiGrid(show: boolean) {
+  public setShowGrid(show: boolean) {
     this.three.getSceneManager().setEtaPhiGrid(show);
   }
 
