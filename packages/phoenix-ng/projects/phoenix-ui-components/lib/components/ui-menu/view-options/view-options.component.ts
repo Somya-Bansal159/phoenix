288b5fb6497c7c0c26bffab7a1d633b5d13b250a
Somya Bansal <somya.bansal.cse20@itbhu.ac.in>
2023-08-28 10:53:02 +0530

    Update view-options.component.ts

--- packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/view-options/view-options.component.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/view-options/view-options.component.ts
@@ -1,30 +1,20 @@
-import { Subscription } from 'rxjs';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PresetView } from 'phoenix-event-display';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 import { EventDisplayService } from '../../../services/event-display.service';
-import { Vector3 } from 'three';
 
 @Component({
   selector: 'app-view-options',
   templateUrl: './view-options.component.html',
   styleUrls: ['./view-options.component.scss'],
 })
-export class ViewOptionsComponent implements OnInit, OnDestroy {
+export class ViewOptionsComponent implements OnInit {
   views: PresetView[];
-  show3DPoints: boolean;
-  origin: Vector3 = new Vector3(0, 0, 0);
-  sub: Subscription;
 
   constructor(private eventDisplay: EventDisplayService) {}
 
   ngOnInit(): void {
     this.views = this.eventDisplay.getUIManager().getPresetViews();
-    this.sub = this.eventDisplay
-      .getThreeManager()
-      .originChanged.subscribe((intersect) => {
-        this.origin = intersect;
-      });
   }
 
   displayView($event: any, view: PresetView) {
@@ -40,16 +30,5 @@ export class ViewOptionsComponent implements OnInit, OnDestroy {
   setGrid(change: MatCheckboxChange) {
     const value = change.checked;
     this.eventDisplay.getUIManager().setShowGrid(value);
-  }
-
-  show3DMousePoints(change: MatCheckboxChange) {
-    this.show3DPoints = change.checked;
-    this.eventDisplay
-      .getUIManager()
-      .show3DMousePoints(this.show3DPoints, this.origin);
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
   }
 }
